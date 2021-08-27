f59752db8cc2b0c9a566f23df35f185ed550cd1e
Vijayant Joshi <vijayantjoshisvvv1822@gmail.com>
2021-08-27 11:57:18 +0530

    HTTPS

--- public/js/main.js
+++ public/js/main.js
@@ -18,7 +18,7 @@ const getInfo = async (event) => {
         datahide.classList.add("data_hide");
     } else {
         try {
-            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=d8efaa2bdb5d350afff2e958ee63329f`;
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=d8efaa2bdb5d350afff2e958ee63329f`;
             const response = await fetch(url);
 
             const data = await response.json();
